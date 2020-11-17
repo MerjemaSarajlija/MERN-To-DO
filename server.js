2182e1f89db684377e488fcb372432964931a715
MerjemaSarajlija <merjema.s@hotmail.com>
2020-11-17 20:15:40 +0100

    heroku

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ mongoose
      });
 }
 
- const port = process.env.PORT || 5000;
+ 
+const port = process.env.PORT || 5000;
  app.listen(port, ()=> console.log(`Server started on port ${port} ` ));
 
