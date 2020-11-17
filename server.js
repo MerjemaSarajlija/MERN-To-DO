c92a5e3c03c0129e83038e97468e328e9791e00d
MerjemaSarajlija <merjema.s@hotmail.com>
2020-11-17 16:25:01 +0100

    make it better

--- server.js
+++ server.js
@@ -30,7 +30,15 @@ mongoose
  app.use('/api/auth', require('./routes/api/auth'));
 
  
- 
+  // SERVE STATIC ASSETS IF IN PRODUCTION
+  if(process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+     });
+}
+
  const port = process.env.PORT || 5000;
  app.listen(port, ()=> console.log(`Server started on port ${port} ` ));
 
